5ea4c045689e21925cd0b6c25fc8a6189598ca6b
ilaf81 <E_T_A_N_scros@yahoo.com>
2019-09-25 20:25:30 -0700

    Initial Check-in

---
+++ react-shopping-cart-master/src/components/Contact/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+class Contact extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      
+        <div>
+          <h1>This is the Contact Page</h1>
+          <ul className="header">
+    
+          </ul>
+          <div className="content">
+           
+
+             
+          </div>
+        </div>
+        <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is post on
+        our forums.
+        </p>
+      </div> 
+     
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contact;
