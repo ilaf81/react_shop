7fccbccc6c35a19acf0a6f37c8450f5deb550c49
ilaf81 <E_T_A_N_scros@yahoo.com>
2019-09-29 17:02:17 -0700

    update folder name

---
+++ react-shopping-cart-master/src/components/Anime/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class Anime extends Component {
+  render() {
+    return (
+       
+      
+      <React.Fragment>
+      <main>
+        
+        <div>
+        <h1>This is page should display Anime</h1>
+
+      
+      </div> 
+      </main>
+                
+
+        
+      </React.Fragment>
+    )
+  }
+}
+
+export default Anime;
